070da9e5a35cbc3163e9c58780835cfe8b680787
nickelproject <nickelproject6432@gmail.com>
2025-08-02 11:08:05 +0330

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,10 @@
+import time
+from tqdm import tqdm
+
 def main():
+    print("Loading...")
+    for _ in tqdm(range(100), bar_format="{l_bar}{bar} {n_fmt}%"):
+        time.sleep(0.03)
     print("Test")
     input("Press Enter to exit...")
 
